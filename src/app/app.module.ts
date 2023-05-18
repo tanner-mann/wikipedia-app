f0507ca7d2779503eb48107b60acb7954ff1c964
Tanner Mann <tm0122@mix.wvu.edu>
2023-05-18 17:56:30 -0400

    Add project files.

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './header/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/header/topbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "wiki-topbar",
+  templateUrl: "topbar.component.html",
+  styleUrls: ["topbar.component.css"]
+})
+
+export class TopbarComponent {
+
+}
